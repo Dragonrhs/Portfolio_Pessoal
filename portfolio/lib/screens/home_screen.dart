b995cb22fbf7f587ad6dcb240179690c222fd928
Ryuske Hideaki Sato <ryuske2009@hotmail.com>
2024-05-19 14:04:25 -0300

    <feat>[]:  competencias row and formaçao image

--- portfolio/lib/screens/home_screen.dart
+++ portfolio/lib/screens/home_screen.dart
@@ -70,29 +70,154 @@ class HomeScreen extends StatefulWidget {
                 style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
               ),
               const SizedBox(height: 50),
+
               SectionCard(key: competenciasKey, title: 'Competências'),
 
               const SizedBox(height: 30),
-              const Text("- Pacote Office avançado", style: TextStyle(fontSize: 16) ),
-              const Text("- Windows avançado", style: TextStyle(fontSize: 16) ),
-              const Text("- Análise de investimentos intermediário", style: TextStyle(fontSize: 16) ),
-              const Text("- Java intermediário", style: TextStyle(fontSize: 16) ),
-              const Text("- Python intermediário", style: TextStyle(fontSize: 16) ),
-              const Text("- C básico", style: TextStyle(fontSize: 16) ),
-              const Text("- C# básico", style: TextStyle(fontSize: 16) ),
-              const Text("- Js básico", style: TextStyle(fontSize: 16) ),
-              const Text("- HTML5 básico", style: TextStyle(fontSize: 16) ),
-              const Text("- CSS básico", style: TextStyle(fontSize: 16) ),
-              const Text("- Flutter básico", style: TextStyle(fontSize: 16) ),
-              const Text("- Linux básico", style: TextStyle(fontSize: 16) ),
-              const Text("- AWS básico", style: TextStyle(fontSize: 16) ),
+              const Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    '- Pacote Office avançado',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 16,
+                    ),
+                  ),
+                  Text(
+                    '- Windows avançado',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 16,
+                    ),
+                  ),
+                  Text(
+                    '- Análise de investimentos intermediário',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 16,
+                    ),
+                  ),
+                  Text(
+                    '- Java intermediário',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 16,
+                    ),
+                  ),
+                  Text(
+                    '- Python intermediário',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 16,
+                    ),
+                  ),
+                  Text(
+                    '- C básico',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 16,
+                    ),
+                  ),
+                  Text(
+                    '- C# básico',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 16,
+                    ),
+                  ),
+                  Text(
+                    '- Js básico',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 16,
+                    ),
+                  ),
+                  Text(
+                    '- HTML5 básico',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 16,
+                    ),
+                  ),
+                  Text(
+                    '- CSS básico',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 16,
+                    ),
+                  ),
+                  Text(
+                    '- Flutter básico',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 16,
+                    ),
+                  ),
+                  Text(
+                    '- Linux básico',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 16,
+                    ),
+                  ),
+                  Text(
+                    '- AWS básico',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 16,
+                    ),
+                  ),
+                ],
+              ),
               const SizedBox(height: 30),
               
                SectionCard(key: formacaoKey,title: 'Formação'),
+
               const SizedBox(height: 30),
-              const Text("- Ensino superior em andamento", style: TextStyle(fontSize: 16) ),
+             
+                     Row(
+                     mainAxisAlignment: MainAxisAlignment.center,
+                     mainAxisSize: MainAxisSize.min,
+                      children: [
+                  Image.network(
+                    'https://media.licdn.com/dms/image/C4D0BAQFgIiUzENGjlw/company-logo_100_100/0/1630492490554/instituto_maua_de_tecnologia_logo?e=1724284800&v=beta&t=0F_r9IpQ62UminajRN6Yp9FWmBi-sqjWsnCFBpbSGd8', // URL da imagem
+                    width: 50,
+                    height: 50,
+                  ),
+                  const SizedBox(width: 10),
+                  const Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Instituto Mauá de Tecnologia',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16,
+                        ),
+                      ),
+                      Text(
+                        'Graduação, Engenharia de Computação',
+                        style: TextStyle(
+                          fontSize: 14,
+                        ),
+                      ),
+                      Text(
+                        '2021 - 2025',
+                        style: TextStyle(
+                          fontSize: 14,
+                          color: Colors.grey,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+
               const SizedBox(height: 30),
+
                SectionCard(key: linksKey,title: 'Links'),
+               
               const SizedBox(height: 30),
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
